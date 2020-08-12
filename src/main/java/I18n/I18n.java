e77a9f86fc5e476af08e11a9384b46a11a016132
Mark Vadeika <markvadeika@gmail.com>
2020-08-12 18:55:40 -0400

    added localization

---
+++ src/main/java/I18n/I18n.java
@@ -0,0 +1,88 @@
+package I18n;
+
+import com.virtualparticle.mc.mckoth.McKoth;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+
+public class I18n {
+
+    private static final Locale ROOT_LOCALE = Locale.ROOT;
+    private static final Locale DEFAULT_LOCALE = Locale.getDefault();
+    private static final String MESSAGES_BASE_NAME = "I18n.messages";
+    private static final I18n INSTANCE = new I18n();
+
+    private final McKoth plugin;
+    private Locale locale;
+    private ResourceBundle bundle;
+    private boolean validLocale;
+
+    public I18n() {
+        this(DEFAULT_LOCALE);
+    }
+
+    public I18n(Locale locale) {
+        this.locale = locale;
+        createBundle(locale);
+        plugin = McKoth.getPlugin();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public static I18n getInstance() {
+        return INSTANCE;
+    }
+
+    private void createBundle(Locale locale) {
+
+        try {
+            bundle = ResourceBundle.getBundle(MESSAGES_BASE_NAME, locale);
+            validLocale = true;
+            plugin.log(Level.INFO, formatString(bundle.getString("loadedLocale"), locale.toString()));
+            return;
+        } catch (MissingResourceException ignored) {
+        }
+
+        try {
+            bundle = ResourceBundle.getBundle(MESSAGES_BASE_NAME, DEFAULT_LOCALE);
+            validLocale = true;
+            plugin.log(Level.WARNING, formatString(
+                    bundle.getString("cantFindLocale"), locale.toString(), DEFAULT_LOCALE.toString()));
+            return;
+        } catch (MissingResourceException ignored) {
+
+        }
+
+        try {
+            bundle = ResourceBundle.getBundle(MESSAGES_BASE_NAME, ROOT_LOCALE);
+            validLocale = true;
+            plugin.log(Level.WARNING, formatString(
+                    bundle.getString("cantFindLocale"), locale.toString(), "ROOT"));
+        } catch (MissingResourceException e) {
+            validLocale = false;
+            plugin.log(Level.SEVERE, "No localization was found!");
+        }
+
+    }
+
+    private String formatString(String msg, String... args) {
+        String s = msg;
+        for (int i = 0; i < args.length; i++) {
+            s = s.replace("{" + i + "}", args[i]);
+        }
+        return s;
+    }
+
+    public String getString(String key, String... args) {
+        return formatString(bundle.getString(key), args);
+    }
+
+}
--- src/main/java/com/virtualparticle/mc/mckoth/McKoth.java
+++ src/main/java/com/virtualparticle/mc/mckoth/McKoth.java
@@ -1,16 +1,21 @@
 package com.virtualparticle.mc.mckoth;
 
+import I18n.I18n;
 import game.Game;
 import game.listeners.GamePlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class McKoth extends JavaPlugin {
 
     private static McKoth plugin;
+    private static I18n i18n = I18n.getInstance();
 
+    private Logger logger;
     private List<Game> games;
 
     @Override
@@ -18,6 +23,7 @@ public final class McKoth extends JavaPlugin {
         // Plugin startup logic
         plugin = this;
         games = new ArrayList<>();
+        logger = Logger.getLogger(this.getClass().getName());
     }
 
     @Override
@@ -31,6 +37,14 @@ public final class McKoth extends JavaPlugin {
 
     public List<Game> getGames() {
         return games;
+    }
+
+    public void log(Level level, String message) {
+        logger.log(level, message);
+    }
+
+    public void log(String message) {
+        log(Level.INFO, message);
     }
 
 }
